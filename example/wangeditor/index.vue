73bd69772fc797078827f96ad32bcf2ea3c90579
xiaochi <875267425@qq.com>
2019-10-17 10:02:17 +0800

    element-component

--- example/wangeditor/index.vue
+++ example/wangeditor/index.vue
@@ -3,7 +3,7 @@
         <h1>wangEditor</h1>
         <component-wang-editor :height="350" @change="change" style="width: 600px;"></component-wang-editor>
         <hr>
-        <component-wang-editor :height="350" @fail="handleFail" @change="change" style="width: 600px;"></component-wang-editor>
+        <component-wang-editor :height="350" @fail="handleFail" @success="handleSuccess" @timeout="handleTimeout" @error="handleError" @change="change" style="width: 600px;"></component-wang-editor>
     </div>
 </template>
 
